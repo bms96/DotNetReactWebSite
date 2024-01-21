897749d6e5ff0772b8b7f209a77179519a98af35
Bryan Santini <bryansantini@yahoo.com>
2024-01-21 13:47:23 -0500

    Created project

---
+++ DotNetReactWebSite/dotnetreactwebsite.client/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import WeatherTable from './WeatherTable/WeatherTable';
+
+
+
+function App() {
+
+    return (
+        <>
+            <WeatherTable/>
+        </>
+    );
+}
+
+export default App;
